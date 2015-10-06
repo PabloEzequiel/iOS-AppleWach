a1371c0adc0b26e150b75f0bd5cc57fb0a2fb853
Pablo Ezequiel Inchausti <inchausti.prod@gmail.com>
2015-10-06 02:51:25 -0300

    first commit

---
+++ HelloAppKitWatch/HelloAppKitWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  HelloAppKitWatch WatchKit Extension
+//
+//  Created by Pablo Ezequiel Inchausti on 6/10/15.
+//  Copyright © 2015 Pablo Ezequiel Inchausti. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# iOS-AppleWach
+
+Ejercicios con el Apple Watch
+
+Descripto en:
+http://iosen.blogspot.com.ar/2015/10/mi-primera-aplicacion-en-el-apple-watch.html
+
+
+
